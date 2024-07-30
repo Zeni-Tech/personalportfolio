52a910ebd827d2ff9e234bbe28e7301f02d55c89
ZAIN ALI CHEEMA <164139373+Zeni-Tech@users.noreply.github.com>
2024-07-30 00:32:33 -0700

    Portfolio is Create in HTML || CSS || Bootstrap || JavaScript

---
+++ index.js
@@ -0,0 +1,34 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+
+//Resume Code:
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_w6h1aux';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
